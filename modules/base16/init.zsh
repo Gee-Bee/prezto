a417e607b2b92bdfa41fdd97848e15b02c020d7b
Gee Bee <greg.bunia@gmail.com>
2016-08-01 23:41:11 +0200

    Change color to base16-eighties

--- modules/base16/init.zsh
+++ modules/base16/init.zsh
@@ -1,4 +1,4 @@
 # Base16 Shell
-BASE16_SCHEME="default"
-BASE16_SHELL="${ZDOTDIR:-$HOME}/.zprezto/modules/base16/base16-shell/base16-$BASE16_SCHEME.dark.sh"
+BASE16_SCHEME="eighties"
+BASE16_SHELL="${ZDOTDIR:-$HOME}/.zprezto/modules/base16/base16-shell/scripts/base16-$BASE16_SCHEME.sh"
 [[ -s $BASE16_SHELL ]] && . $BASE16_SHELL
